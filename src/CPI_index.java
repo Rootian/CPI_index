c3afdb04d7602ae9d472b96198245d6d7635240d
AaronKB <AaronKB@DESKTOP-6A14ULK>
2019-04-01 18:14:50 +0800

    cal_center

--- src/CPI_index.java
+++ src/CPI_index.java
@@ -1,16 +1,41 @@
 import java.awt.geom.Point2D;
 import java.io.*;
 import java.util.ArrayList;
+
+
 public class CPI_index {
 	private static final double EARTH_RADIUS = 6371393; // 平均半径,单位：m
+	static clustered_point[] centers; //聚类中心集
+	static int num_cluster; //聚类中心的个数
 
 	public static void main(String[] args) {
 		// 读取CSV文件中的聚类点数据
 		String filePath = new String("bin\\512x512.csv");
 		ReadCSV file_csv = new ReadCSV(filePath);
 		ArrayList<clustered_point> points = file_csv.read(filePath);
-//		System.out.printf("test %d",points.get(3).index);
+//		System.out.printf("test %d",file_csv.num_cluster);
+		num_cluster = file_csv.num_cluster;
+		centers = new clustered_point[file_csv.num_cluster];
+		centers = cal_centers(points);
 		
+	}
+	public static clustered_point[] cal_centers(ArrayList<clustered_point> points) {
+		//计算聚类点中心
+		double X = 0, Y = 0, Z = 0;
+		
+		for(clustered_point cpoint : points) {
+			double lat,lon,x,y,z;
+			lon = cpoint.point.getX() * Math.PI / 180;
+			lat = cpoint.point.getY() * Math.PI / 180;
+			x = Math.cos(lat) * Math.cos(lon);  
+	        y = Math.cos(lat) * Math.sin(lon);  
+	        z = Math.sin(lat);  
+	        X += x;  
+	        Y += y;  
+	        Z += z; 
+		}
+		
+		return null;
 	}
 	
 	/**
@@ -40,6 +65,7 @@ public class CPI_index {
 
 class ReadCSV{
 	String filePath;
+	int num_cluster;
 	public ReadCSV(String file) {
 		filePath = file;
 	}
@@ -49,8 +75,14 @@ class ReadCSV{
 			BufferedReader reader = new BufferedReader(new FileReader(filePath));
 			ArrayList<clustered_point> points = new ArrayList<clustered_point>();
 			String line = null;
+			int flag = 1;
 			while((line = reader.readLine()) != null) {
 				String[] item = line.split(",");
+				if (flag == 1) {
+					//获取聚类个数
+					num_cluster = Integer.parseInt(item[2]);
+					flag = 0;
+				}
 				Point2D point = new Point2D.Double(Double.parseDouble(item[0]),Double.parseDouble(item[1]));
 				clustered_point p = new clustered_point(point, Integer.parseInt(item[2]));
 				points.add(p);
